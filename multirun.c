10e32f7dbc935f1b5fb732697d42e1c55f00f9c9
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2016-12-14 16:33:25 +0100

    Version that uses sh

--- multirun.c
+++ multirun.c
@@ -19,7 +19,7 @@ int main(int argc, const char** argv) {
         pid_t pid = fork();
         if (pid == 0) {
             sub_exec(argv[i]);
-            exit(-1);
+            exit(-1); // should not happen
         } else {
             subprocess new_sub;
             new_sub.pid = pid;
@@ -30,28 +30,8 @@ int main(int argc, const char** argv) {
 }
 
 void sub_exec(const char* command) {
-    char* command_copy;
-    char* current;
-    char* args[100];
-    int argn = 0;
     int ret;
-    
-    command_copy = malloc(sizeof(char) * (strlen(command) + 1));
-    strcpy(command_copy, command);
-    current = command_copy;
-    
-    for (int i = 0; i < 100; i++) {
-        char* token = strtok(current, " ");
-        current = NULL;
-        if (token == NULL)
-            break;
-        args[i] = malloc(sizeof(char) * (strlen(token) + 1));
-        strcpy(args[i], token);
-        argn += 1;
-    }
-    args[argn] = NULL;
-    
-    ret = execvp(args[0], args);
+    ret = execlp("sh", "sh", "-c", command, (char*) NULL);
     if (ret != 0) {
         printf("Error launching the subprocess: %s\n", strerror(errno));
         exit(-1);
