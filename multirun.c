b516cf27791c57c307ec03d2b7373e0408cec090
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2016-12-14 16:42:22 +0100

    Now wait for all children to terminate

--- multirun.c
+++ multirun.c
@@ -13,20 +13,25 @@ void sub_exec(const char * command);
 int main(int argc, const char** argv) {
     
     subprocess* subprocesses;
-    subprocesses = malloc(sizeof(subprocess) * argc - 1);
+    int nbr = argc - 1;
+    subprocesses = malloc(sizeof(subprocess) * nbr);
+    int wstatus;
     
-    for (int i = 1; i < argc; i++) {
+    for (int i = 0; i < nbr; i++) {
         pid_t pid = fork();
         if (pid == 0) {
-            sub_exec(argv[i]);
+            sub_exec(argv[i + 1]);
             exit(-1); // should not happen
         } else {
             subprocess new_sub;
             new_sub.pid = pid;
-            subprocesses[i - 1] = new_sub;
+            subprocesses[i] = new_sub;
         }
     }
-
+    
+    for (int i = 0; i < nbr; i++) {
+        wait(&wstatus);
+    }
 }
 
 void sub_exec(const char* command) {
