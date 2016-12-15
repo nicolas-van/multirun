887e5c6bc63c4fbb89d656336309d0a2c623aa32
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2016-12-15 12:59:39 +0100

    Updated package.sh

--- package.sh
+++ package.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
+rm multirun
 cp Dockerfile.ubuntu Dockerfile
 docker build -t multirun-build-ubuntu:latest .
 docker run -it --rm -v /tmp/dist:/dist multirun-build-ubuntu:latest
 mv /tmp/dist/multirun .
 tar zcvf multirun-ubuntu-$1.tar.gz multirun
+rm Dockerfile
 
+rm multirun
 cp Dockerfile.alpine Dockerfile
 docker build -t multirun-build-alpine:latest .
 docker run -it --rm -v /tmp/dist:/dist multirun-build-alpine:latest
 mv /tmp/dist/multirun .
 tar zcvf multirun-alpine-$1.tar.gz multirun
-
 rm Dockerfile
+
+rm multirun
